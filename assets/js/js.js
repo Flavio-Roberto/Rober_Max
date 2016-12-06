89d1c67c367260aa75cce3e568195c29b8c36a5d
Flavio Roberto <flavioroberto.leite@hotmail.com>
2016-12-05 23:59:52 -0200

    Rober_MAx

---
+++ assets/js/js.js
@@ -0,0 +1,200 @@
+// JavaScript Document
+$(function() {
+                $('#activator').click(function(){
+                    $('#overlay').fadeIn('fast',function(){
+                        $('#box').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose').click(function(){
+                    $('#box').animate({'top':'-2000%'},500,function(){
+                        $('#overlay').fadeOut('fast');
+                    });
+                });
+
+            });
+			
+			 $(function() {
+                $('#activator2').click(function(){
+                    $('#overlay2').fadeIn('fast',function(){
+                        $('#box2').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose2').click(function(){
+                    $('#box2').animate({'top':'-2000%'},500,function(){
+                        $('#overlay2').fadeOut('fast');
+                    });
+                });
+
+            });
+			
+			$(function() {
+                $('#activator3').click(function(){
+                    $('#overlay3').fadeIn('fast',function(){
+                        $('#box3').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose3').click(function(){
+                    $('#box3').animate({'top':'-2000%'},500,function(){
+                        $('#overlay3').fadeOut('fast');
+                    });
+                });
+
+            });
+			
+			$(function() {
+                $('#activator4').click(function(){
+                    $('#overlay4').fadeIn('fast',function(){
+                        $('#box4').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose4').click(function(){
+                    $('#box4').animate({'top':'-2000%'},500,function(){
+                        $('#overlay4').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator5').click(function(){
+                    $('#overlay5').fadeIn('fast',function(){
+                        $('#box5').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose5').click(function(){
+                    $('#box5').animate({'top':'-2000%'},500,function(){
+                        $('#overlay5').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator6').click(function(){
+                    $('#overlay6').fadeIn('fast',function(){
+                        $('#box6').animate({'top':'1%'},500);
+                    });
+                });
+                $('#boxclose6').click(function(){
+                    $('#box6').animate({'top':'-2000%'},500,function(){
+                        $('#overlay6').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator7').click(function(){
+                    $('#overlay7').fadeIn('fast',function(){
+                        $('#box7').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose7').click(function(){
+                    $('#box7').animate({'top':'-2000%'},500,function(){
+                        $('#overlay7').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator8').click(function(){
+                    $('#overlay8').fadeIn('fast',function(){
+                        $('#box8').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose8').click(function(){
+                    $('#box8').animate({'top':'-2000%'},500,function(){
+                        $('#overlay8').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator9').click(function(){
+                    $('#overlay9').fadeIn('fast',function(){
+                        $('#box9').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose9').click(function(){
+                    $('#box9').animate({'top':'-2000%'},500,function(){
+                        $('#overlay9').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator10').click(function(){
+                    $('#overlay10').fadeIn('fast',function(){
+                        $('#box10').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose10').click(function(){
+                    $('#box10').animate({'top':'-2000%'},500,function(){
+                        $('#overlay10').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator11').click(function(){
+                    $('#overlay11').fadeIn('fast',function(){
+                        $('#box11').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose11').click(function(){
+                    $('#box11').animate({'top':'-2000%'},500,function(){
+                        $('#overlay11').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator12').click(function(){
+                    $('#overlay12').fadeIn('fast',function(){
+                        $('#box12').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose12').click(function(){
+                    $('#box12').animate({'top':'-2000%'},500,function(){
+                        $('#overlay12').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator13').click(function(){
+                    $('#overlay13').fadeIn('fast',function(){
+                        $('#box13').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose13').click(function(){
+                    $('#box13').animate({'top':'-2000%'},500,function(){
+                        $('#overlay13').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator14').click(function(){
+                    $('#overlay14').fadeIn('fast',function(){
+                        $('#box14').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose14').click(function(){
+                    $('#box14').animate({'top':'-2000%'},500,function(){
+                        $('#overlay14').fadeOut('fast');
+                    });
+                });
+
+            });
+			$(function() {
+                $('#activator15').click(function(){
+                    $('#overlay15').fadeIn('fast',function(){
+                        $('#box15').animate({'top':'2%'},500);
+                    });
+                });
+                $('#boxclose15').click(function(){
+                    $('#box15').animate({'top':'-2000%'},500,function(){
+                        $('#overlay15').fadeOut('fast');
+                    });
+                });
+
+            });
+       
